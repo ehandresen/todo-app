0dae4d6ef71d9220c015d49c53f7495ae282d8fd
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-07-31 06:10:18 +0200

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import TodoList from "./components/TodoList"
-
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <TodoList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 
 function TodoList() {
+  const [input, setInput] = useState("");
+  console.log(input);
+
   return (
     <div className="bg-stone-300 p-5 h-screen">
-        <h1 className="text-3xl my-3 text-center text-red-600">Todo List</h1>
+      <h1 className="text-3xl my-3 text-center text-red-600">Todo List</h1>
 
-        <div className="flex justify-center">
-            <input className="p-2 rounded-md" type="text" placeholder="Add item" />
-            <button className="bg-blue-500 text-white py-1 px-3 border rounded-md">Add</button>
-        </div>
+      <div className="flex justify-center">
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          className="p-2 rounded-md"
+          type="text"
+          placeholder="Add item"
+        />
+        <button className="bg-blue-500 text-white py-1 px-3 border rounded-md">
+          Add
+        </button>
+      </div>
+
+      {/* Todo Items */}
+      <div>
+        <ul>
+          <li>{input}</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
