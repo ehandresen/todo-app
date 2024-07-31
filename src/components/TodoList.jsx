29404ac606c1ce3f0e02d4dab25b57abc84fc11f
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-07-31 18:47:21 +0200

    add task to list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
 function TodoList() {
+  const [tasks, setTasks] = useState(["task one", "task two"]);
   const [input, setInput] = useState("");
-  console.log(input);
+
+  const handleClick = () => {
+    setTasks([...tasks, input]);
+    console.log(tasks);
+  };
 
   return (
     <div className="bg-stone-300 p-5 h-screen">
-      <h1 className="text-3xl my-3 text-center text-red-600">Todo List</h1>
+      <h1 className="text-5xl my-3 text-center text-red-600">Todo List</h1>
 
       <div className="flex justify-center">
         <input
-          onChange={(e) => setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.value)} // get user input
           className="p-2 rounded-md"
           type="text"
           placeholder="Add item"
         />
-        <button className="bg-blue-500 text-white py-1 px-3 border rounded-md">
+        <button
+          onClick={handleClick} // add tasks
+          className="bg-blue-500 text-white py-1 px-3 border rounded-md"
+        >
           Add
         </button>
       </div>
@@ -23,7 +31,9 @@ function TodoList() {
       {/* Todo Items */}
       <div>
         <ul>
-          <li>{input}</li>
+          {tasks.map((task, i) => (
+            <li key={i}>{task}</li>
+          ))}
         </ul>
       </div>
     </div>
